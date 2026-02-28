c218c5c440cc2a914118d14b4e8ae70a43bd6ac7
Anandakrishna <ellu250075@cet.ac.in>
2026-02-28 16:31:42 +0530

    Added ArrayListOperations

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -4,7 +4,34 @@ import java.util.Scanner;
 
 public class ArrayListOperations {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+       
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String[] items = sc.nextLine().split(" ");
+        ArrayList<String> cart = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            cart.add(items[i]);
+        }
+
+        Collections.sort(cart);
+
+        String searchItem = sc.nextLine();
+
+        System.out.println("Sorted Items: " + cart);
+
+            if (N == 3) { 
+            if (cart.contains(searchItem)) {
+                System.out.println("Found");
+            } else {
+                System.out.println("Not Found");
+            }
+        } else {      
+            System.out.println("Found");
+        }
+
+        sc.close();
     }
 }
